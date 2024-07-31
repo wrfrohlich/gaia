837be6542c2b1881471d9b98bc9d33e9db4e3c8e
William da Rosa Frohlich <william_r_f@hotmail.com>
2024-07-30 23:11:43 -0300

    Add vectors for correlations
    Signed-off-by: William da Rosa Frohlich <william_r_f@hotmail.com>

--- gaia/correlation.py
+++ gaia/correlation.py
@@ -69,32 +69,110 @@ class Correlation:
             "acc_x", "acc_y", "acc_z",
             "gyro_x", "gyro_y", "gyro_z",
             "roll", "pitch", "yaw",
-            "r should.X", "r should.Y", "r should.Z",
-            "l should.X", "l should.Y", "l should.Z",
-            "sacrum s.X", "sacrum s.Y", "sacrum s.Z",
-            "PO.X", "PO.Y", "PO.Z"
+            "r should_x", "r should_y", "r should_z",
+            "l should_x", "l should_y", "l should_z",
+            "sacrum s_x", "sacrum s_y", "sacrum s_z",
+            "PO_x", "PO_y", "PO_z"
         ]]
         data_lower_01 = data[[
             "acc_x", "acc_y", "acc_z",
             "gyro_x", "gyro_y", "gyro_z",
             "roll", "pitch", "yaw",
-            "r knee 1.X", "r knee 1.Y", "r knee 1.Z",
-            "l knee 1.X", "l knee 1.Y", "l knee 1.Z",
-            "r mall.X", "r mall.Y", "r mall.Z",
-            "l mall.X", "l mall.Y", "l mall.Z"
+            "r knee 1_x", "r knee 1_y", "r knee 1_z",
+            "l knee 1_x", "l knee 1_y", "l knee 1_z",
+            "r mall_x", "r mall_y", "r mall_z",
+            "l mall_x", "l mall_y", "l mall_z"
         ]]
         data_lower_02 = data[[
             "acc_x", "acc_y", "acc_z",
             "gyro_x", "gyro_y", "gyro_z",
             "roll", "pitch", "yaw",
-            "r heel.X", "r heel.Y", "r heel.Z",
-            "l heel.X", "l heel.Y", "l heel.Z",
-            "r met.X", "r met.Y", "r met.Z",
-            "l met.X", "l met.Y", "l met.Z"
+            "r heel_x", "r heel_y", "r heel_z",
+            "l heel_x", "l heel_y", "l heel_z",
+            "r met_x", "r met_y", "r met_z",
+            "l met_x", "l met_y", "l met_z"
         ]]
         self.gen_corr_matrix(data_upper, name="upper_body")
         self.gen_corr_matrix(data_lower_01, name="lower_body_01")
         self.gen_corr_matrix(data_lower_02, name="lower_body_02")
+
+        df = pd.DataFrame()
+        df["roll"] = data["roll"]
+        df["pitch"] = data["pitch"]
+        df["yaw"] = data["yaw"]
+        df["acc"] = self.get_vector(data, "acc")
+        df["gyro"] = self.get_vector(data, "gyro")
+        df["r should"] = self.get_vector(data, "r should")
+        df["l should"] = self.get_vector(data, "l should")
+        df["sacrum"] = self.get_vector(data, "sacrum s")
+        df["PO"] = self.get_vector(data, "PO")
+        df["r knee 1"] = self.get_vector(data, "r knee 1")
+        df["l knee 1"] = self.get_vector(data, "l knee 1")
+        df["r mall"] = self.get_vector(data, "r mall")
+        df["l mall"] = self.get_vector(data, "l mall")
+        df["r heel"] = self.get_vector(data, "r heel")
+        df["l heel"] = self.get_vector(data, "l heel")
+        df["r met"] = self.get_vector(data, "r met")
+        df["l met"] = self.get_vector(data, "l met")
+
+        self.gen_corr_matrix(df, name="vectors")
+
+        # Função para calcular correlação cruzada
+        def cross_correlation(a, b, lag=0):
+            return np.corrcoef(a[:-lag or None], b[lag:])[0, 1]
+
+        # Calcular correlações cruzadas para diferentes defasagens
+        max_lag = 20  # Número máximo de desfasagens a considerar
+        cross_corrs = [cross_correlation(data['acc_x'], data['r should_x'], lag) for lag in range(max_lag)]
+
+        # Plotar correlação cruzada
+        plt.figure(figsize=(12, 6))
+        plt.plot(range(max_lag), cross_corrs, marker='o')
+        plt.xlabel('Lag')
+        plt.ylabel('Cross-Correlation')
+        plt.title('Correlação Cruzada entre acc_x e r should.X')
+        plt.savefig(f'{self.path_matrix}/bluba1.png')
+        plt.clf()
+
+        # Calcular correlações cruzadas para diferentes defasagens
+        max_lag = 20  # Número máximo de desfasagens a considerar
+        cross_corrs = [cross_correlation(data['acc_y'], data['r should_y'], lag) for lag in range(max_lag)]
+
+        # Plotar correlação cruzada
+        plt.figure(figsize=(12, 6))
+        plt.plot(range(max_lag), cross_corrs, marker='o')
+        plt.xlabel('Lag')
+        plt.ylabel('Cross-Correlation')
+        plt.title('Correlação Cruzada entre acc_y e r should.X')
+        plt.savefig(f'{self.path_matrix}/bluba2.png')
+        plt.clf()
+        # Calcular correlações cruzadas para diferentes defasagens
+        max_lag = 20  # Número máximo de desfasagens a considerar
+        cross_corrs = [cross_correlation(data['gyro_x'], data['r should_x'], lag) for lag in range(max_lag)]
+
+        # Plotar correlação cruzada
+        plt.figure(figsize=(12, 6))
+        plt.plot(range(max_lag), cross_corrs, marker='o')
+        plt.xlabel('Lag')
+        plt.ylabel('Cross-Correlation')
+        plt.title('Correlação Cruzada entre gyro_x e r should.X')
+        plt.savefig(f'{self.path_matrix}/bluba3.png')
+        plt.clf()
+
+    def get_vector(self, data, label):
+
+        value = data.apply(lambda row: self.create_vector(row, label), axis=1)
+
+        # Calcular a magnitude dos vetores
+        value = value.apply(np.linalg.norm)
+        return value
+
+
+    # Função para criar um vetor tridimensional
+    def create_vector(self, row, prefix):
+        return np.array([row[f"{prefix}_x"], row[f"{prefix}_y"], row[f"{prefix}_z"]])
+
+
 
     def gen_corr_matrix(self, data, name=""):
         corr_matrix_pearson = data.corr(method='pearson')
@@ -195,7 +273,7 @@ class Correlation:
                 plt.figure(figsize=(14, 5))
                 plt.plot(lags, correlation)
                 plt.title(f'Cross Correlation between {col1} and {col2}')
-                plt.xlabel('Lags')
-                plt.ylabel('Correlation')
+                plt_xlabel('Lags')
+                plt_ylabel('Correlation')
                 plt.savefig(f'{self.path_cross}/cross_corr_{col1}_{col2}.png')
                 plt.clf()
